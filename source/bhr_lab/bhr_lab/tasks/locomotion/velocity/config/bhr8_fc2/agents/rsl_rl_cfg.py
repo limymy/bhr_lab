6ed4e483e49dd2b2429579bd531145a0622f3f47
hexb66 <41655394+hexb66@users.noreply.github.com>
2025-05-24 09:33:00 +0800

    Fix configclass missing for Bhr8Fc2PPORunnerCfg

--- source/bhr_lab/bhr_lab/tasks/locomotion/velocity/config/bhr8_fc2/agents/rsl_rl_cfg.py
+++ source/bhr_lab/bhr_lab/tasks/locomotion/velocity/config/bhr8_fc2/agents/rsl_rl_cfg.py
@@ -1,14 +1,17 @@
 from bhr_lab.tasks.locomotion.velocity.config.bhr_base.agents.rsl_rl_cfg import (
-    BasePPORunnerCfg, RecurrentPPORunnerCfg, MirrorPPORunnerCfg
+    BasePPORunnerCfg, RecurrentPPORunnerCfg, MirrorPPORunnerCfg, configclass
 )
 
+@configclass
 class Bhr8Fc2BasePPORunnerCfg(BasePPORunnerCfg):
     """Bhr8 Fc2 PPO actor critic config."""
     experiment_name = "bhr8_fc2_base"
 
+@configclass
 class Bhr8Fc2RecurrentPPORunnerCfg(RecurrentPPORunnerCfg):
     experiment_name = "bhr8_fc2_recurrent"
 
+@configclass
 class Bhr8Fc2NoArmMirrorPPORunnerCfg(MirrorPPORunnerCfg):
     experiment_name = "bhr8_fc2_noarm_mirror"
 
